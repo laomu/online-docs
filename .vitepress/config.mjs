7cb74dc6e5e6554f661e3e5fd4606112a56ed254
DAMU <1007821300@qq.com>
2025-05-02 17:52:02 +0800

    初始化仓库

---
+++ .vitepress/config.mjs
@@ -0,0 +1,118 @@
+import { defineConfig } from 'vitepress'
+import { set_sidebar } from './utils/auto_sidebar.mjs'
+// import { generateSidebar } from 'vitepress-sidebar';
+// import { withSidebar } from 'vitepress-sidebar';
+
+// const vitePressOptions = {};
+
+// const vitePressSidebarOptions = [
+//   {
+//     documentRootPath: '',
+//     scanStartPath: '/programming/',
+//     basePath: '/programming/',
+//     resolvePath: '/programming/Python',
+//     useTitleFromFileHeading: true
+//   },
+//   {
+//     documentRootPath: '',
+//     scanStartPath: '/webpages/',
+//     resolvePath: '/webpages/',
+//     useTitleFromFrontmatter: true
+//   }
+// ];
+
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  base: '/documentation',
+  head: [["link", { rel: "icon", href: "/logo_dark.png" }]],
+  title: "牧码人",
+  description: "希望你见过所有的浑浊，眼里还有星辰，大牧莫邪的在线文档库",
+  themeConfig: {
+    // 配置项目的基础目录
+    // base: 'docs',
+    // logo配置
+    logo: {
+      light: "/logo_dark.png",
+      dark: "/logo_light.png"
+    },
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      {
+        text: '编程语言', items: [
+          { text: "Python", link: "/programming/Python" }
+        ]
+      },
+      {
+        text: '前端开发', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: 'WEB开发', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: '数据库', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: '虚拟化', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: "AI大模型", items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: '工具软件', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      },
+      {
+        text: '开源项目', items: [
+          { text: "待升级..", link: "/" }
+        ]
+      }
+    ],
+
+    // sidebar: withSidebar(vitePressOptions, vitePressSidebarOptions),
+    sidebar: {
+      "/programming/Python": set_sidebar('/programming/Python/'),
+    },
+
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
+      { icon: 'gitee', link: 'https://gitee.com/mouwenbin' }
+    ],
+
+    footer: {
+      copyright: "CopyRight © 2025-present DAMU",
+      message: "大牧老师的在线文档库 | 希望你见过所有的浑浊，眼里还有星辰"
+    },
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档"
+          }
+        },
+        modal: {
+          noResultsText: "无法找到相关结果",
+          resetButtonTitle: "清除查询条件",
+          footer: {
+            selectText: "选择",
+            navigateText: "切换"
+          }
+        },
+      }
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,29 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "牧码人 Coder"
+  text: "大牧老师的在线文档库"
+  # tagline: "希望你见过所有的浑浊，眼里还有星辰"
+  tagline: "上天让你错过谁都有理由，才把最好的留到最后"
+  image:
+    src: /background.svg
+    alt: 大牧的在线文档库
+  actions:
+    - theme: brand
+      text: 简书记忆
+      link: https://www.jianshu.com/
+    - theme: alt
+      text: 大牧的抖音
+      link: https://www.douyin.com/user/MS4wLjABAAAAt5velxXRJYNkq-YwwKUK16vaOIOuG2_igCn5nuQFPDZjjgMUpEkaxY6X5MB3MAuu?from_tab_name=main
+
+features:
+  - title: "内容丰富"
+    details: "文档库集成研发、云计算、运维、网络、网络安全、AI大模型等多方面专业教程，提供全方位的技术支持"
+  - title: "持续集成"
+    details: "紧跟前沿技术发展趋势，及时更新最新的技术动向和技术文档，提供最及时的解决方案"
+  - title: "前沿技术"
+    details: "大牧老师随时更新最前沿的开发技术和开发技巧，大家一起共同交流和进步噢" 
+---
+
